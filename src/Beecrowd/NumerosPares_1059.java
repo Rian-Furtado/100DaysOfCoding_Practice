115fd915e661ece1e1a3e7b9941467a18982814e
Rian Furtado <rianoliveirafurtado@gmail.com>
2023-01-12 19:02:01 -0300

    Dia 12 - Beecrowd

---
+++ src/Beecrowd/NumerosPares_1059.java
@@ -0,0 +1,13 @@
+package Beecrowd;
+
+public class NumerosPares_1059 {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 100; i++) {
+
+            if(i % 2 == 0){
+                System.out.println(i);
+            }
+        }
+    }
+}
