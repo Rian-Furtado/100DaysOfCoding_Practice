e7d1c2afb8aad00055fb777c005ce5d3ed2edb3c
Rian Furtado <rianoliveirafurtado@gmail.com>
2023-02-07 23:17:12 -0300

    Dia 38 - Beecrowd

--- src/Beecrowd/Maior_Posicao_1080.java
+++ src/Beecrowd/Maior_Posicao_1080.java
@@ -5,25 +5,24 @@ import java.util.Scanner;
 public class Maior_Posicao_1080 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] nums = new int[100];
+
+        int[] valores = new int[101];
 
 
-        for (int i = 0; i < nums.length; i++) {
-            nums[i] = sc.nextInt();
+        for (int i = 1; i < valores.length; i++) {
+            valores[i] = sc.nextInt();
         }
 
-        int maior = nums[0];
-        int position = 0;
+        int maior = valores[0];
+        int posicao = 0;
 
-        for (int i = 0; i < nums.length; i++) {
-
-            if(nums[i] > maior){
-                maior = nums[i];
-                position = i;
+        for (int i = 1; i < valores.length; i++) {
+            if (valores[i] > maior) {
+                maior = valores[i];
+                posicao = i;
             }
         }
-
         System.out.println(maior);
-        System.out.println(position);
+        System.out.println(posicao);
     }
 }
