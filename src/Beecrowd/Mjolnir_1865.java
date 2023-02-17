d6c1b5965eccccd066de59d2c871aee3705f61b2
Rian Furtado <rianoliveirafurtado@gmail.com>
2023-02-16 21:56:13 -0300

    Dia 42 - Beecrowd

---
+++ src/Beecrowd/Mjolnir_1865.java
@@ -0,0 +1,20 @@
+package Beecrowd;
+
+import java.util.Scanner;
+
+public class Mjolnir_1865 {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        int C = leitor.nextInt();
+        String nome;
+        int N;
+
+        for (int i = 0; i < C; i++) {
+            nome = leitor.next();
+            N = leitor.nextInt();
+            if (nome.equals("Thor")) System.out.println("Y");
+            else System.out.println("N");
+        }
+    }
+
+}
